1cfc5ab35f8916377b072bed8c40957aa676c666
nhchung11 <99390111+nhchung11@users.noreply.github.com>
2024-10-28 00:10:29 +0700

    Update apb_slave.v

--- RTL/apb_slave.v
+++ RTL/apb_slave.v
@@ -1,96 +1,64 @@
-module apb
-    (
-	    input               PCLK,           // Clock
-		input               PRESETn,        // Active-low reset
-		input               PSELx,          // Select
-		input               PWRITE,         // Direction: 
-                                                // 1 -> write
-                                                // 0 -> read
-		input               PENABLE,        // Enable
-		input       [7:0]   PADDR,          // APB address bus, 8 bits
-		input       [7:0]   PWDATA,         // Write data
-        input       [7:0]   status_reg,      
-        input       [7:0]   receive_reg,
+module apb #(parameter data_size = 8)
+(
+        input pclk, pwrite, psel, penable, presetn,
+        input [data_size-1:0] pwdata, paddr,
+        input [data_size-1:0] rdata,
+        input full, empty, almost_full, almost_empty,
 
-		output              PREADY,         // Ready
-                                            // Slave can complete the transfer at next PCLK posedge
-        output reg  [7:0]   PRDATA,
-        output reg  [7:0]   transmit_reg, 
-        output reg  [7:0]   command_reg,
-        output reg  [7:0]   prescale_reg,
-        output reg  [7:0]   address_reg
-	);
+        output logic new_data,
+        output logic [data_size-1:0] prdata,
+        output logic [data_size-1:0] config_reg,        // 0
+        output logic [data_size-1:0] control_reg,       // 1
+        output [data_size-1:0] wdata,
+        output pready
+);
 
-    reg                     TX_empty;
-    reg                     TX_full;
-    reg                     RX_full;
-    reg                     RX_empty;
-    // reg [2:0]               reg_map;
+logic [data_size-1:0] input_reg;        // 2
+logic [data_size-1:0] status_reg;       // 3
+logic [data_size-1:0] output_reg;       // 4
 
+assign pready = ~full;
+assign wdata = input_reg;
 
-    // Input status
-    always @* begin
-        TX_full             = status_reg[7];
-        TX_empty            = status_reg[6];
-        RX_full             = status_reg[5];
-        RX_empty            = status_reg[4];
-    end
+always_comb begin
+        if (paddr == 8'd3) prdata = status_reg;
+        else prdata = output_reg;
+end
 
-    // READY
-    assign PREADY = ((PENABLE == 1'b1) & (PSELx == 1'b1)) ? 1'b1 : 1'b0;
-    
-    always @(posedge PCLK, negedge PRESETn) begin
-        if (!PRESETn) begin
-            command_reg                 <= 8'b00000000;
-            address_reg                 <= 8'b0;
-            prescale_reg                <= 8'b0;
+always_ff @(posedge pclk or negedge presetn) begin
+        if (~presetn) begin
+                output_reg <= 0;
+                status_reg <= 0;
+        end else begin
+                output_reg <= rdata;
+                status_reg <= {4'b0, empty, full, almost_empty, almost_full};
         end
-        else begin 
-            case (PADDR)
-                // Write to Prescale register
-                1: begin
-                    if ((PWRITE == 1) && (PSELx == 1) && (PENABLE == 1))
-                        prescale_reg    <= PWDATA;
-                end
+end
 
-                // Write to slave address
-                2: begin
-                    if ((PWRITE == 1) && (PSELx == 1) && (PENABLE == 1))
-                        address_reg     <= PWDATA;
+always_ff @(posedge pclk or negedge presetn) begin
+    if (~presetn) begin
+        input_reg       <= 8'b0;
+        control_reg     <= 8'b0;
+        config_reg      <= 8'b0;
+        new_data        <= 1'b0;
+    end
+    else begin
+        if (psel & penable) begin
+                if (pwrite) begin
+                        if (paddr == 8'd0)              config_reg      <= pwdata;
+                        else if (paddr == 8'd2) begin
+                                                        input_reg       <= pwdata;
+                                                        new_data        <= 1'b1;
+                        end
+                        else if (paddr == 8'd1)         control_reg     <= pwdata;
                 end
-
-                // Read from status register
-                3: begin
-                    if ((PWRITE == 0) && (PSELx == 1) && (PENABLE == 1))
-                        PRDATA          <= status_reg;
-                end
-
-                // Write to transmit register
-                4: begin
-                    if ((PWRITE == 1) && (PSELx == 1) && (PENABLE == 1)) begin
-                        transmit_reg    <= PWDATA;
-                        command_reg     <= 11010000;
-                    end
-                    if (PSELx == 0)
-                        command_reg     <= 10010000;
-                end
-
-                // Read from receive register
-                5: begin
-                    if (PSELx == 0)
-                        command_reg     <= 10010000;
-                    else if ((PWRITE == 0) && (PSELx == 1) && (PENABLE == 1)) begin
-                        PRDATA          <= receive_reg;
-                        command_reg     <= 10110000;
-                    end
-                end
-                
-                // Write to Command regisger
-                6: begin
-                    if ((PWRITE == 1) && (PSELx == 1) && (PENABLE == 1))
-                        command_reg     <= PWDATA;
-                end
-            endcase            
+        end
+        else begin
+                input_reg       <= input_reg;
+                control_reg     <= control_reg;
+                config_reg      <= config_reg;
+                new_data        <= 1'b0;
         end
     end
+end
 endmodule
